c6fe37b92e455bbc75dc724e1db93af74d512fa8
Virtual Connect Group <kyle@virtualconnectgroup.com>
2022-03-30 12:45:16 -0700

    Initital Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+    import { swapCameraComponent } from './camera-swap.js'
+
+    AFRAME.registerComponent('swap-camera', swapCameraComponent)
+
+    // AFRAME.registerComponent('realityready', realityreadyComponent)
