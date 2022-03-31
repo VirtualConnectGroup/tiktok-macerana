26f1f8bb41837ff2852084195cf03ebf3abcfcd5
Virtual Connect Group <kyle@virtualconnectgroup.com>
2022-03-31 08:26:05 -0700

    Remove extra buttons

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-    import { swapCameraComponent } from './camera-swap.js'
-
-    AFRAME.registerComponent('swap-camera', swapCameraComponent)
-
-    // Copyright (c) 2022 8th Wall, Inc.
+    // import { swapCameraComponent } from './camera-swap.js'
     //
-    // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
-    // is loaded, and before body.html is loaded.
-    // Register custom A-Frame components in app.js before the scene in body.html has loaded.
-    import { tapPlaceComponent } from './tap-place'
-    AFRAME.registerComponent('tap-place', tapPlaceComponent)
+    // AFRAME.registerComponent('swap-camera', swapCameraComponent)
 
-    // AFRAME.registerComponent('realityready', realityreadyComponent)
+    import './index.css'
+    import { nextButtonComponent } from './next-button.js'
+    AFRAME.registerComponent('next-button', nextButtonComponent())
