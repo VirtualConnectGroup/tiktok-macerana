a98d55eea7e532f5a480e3b5a3929cc190ef756f
Virtual Connect Group <kyle@virtualconnectgroup.com>
2022-04-03 10:02:15 -0700

    Commit before trying a different GLB

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
     // import { swapCameraComponent } from './camera-swap.js'
     //
     // AFRAME.registerComponent('swap-camera', swapCameraComponent)
-
-    import './index.css'
     import { nextButtonComponent } from './next-button.js'
     AFRAME.registerComponent('next-button', nextButtonComponent())
